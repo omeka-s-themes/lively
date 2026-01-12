95da9f8f5fdb41bb83b58eaacbb2d08bd0a3f1f6
Nelson Amaya <nelsonamaya82@gmail.com>
2026-01-12 12:11:53 -0500

    Advanced search form: Remove Button issue (addresses #24)

--- asset/js/advanced-search.js
+++ asset/js/advanced-search.js
@@ -3,34 +3,49 @@ const advancedSearchScripts = () => {
     const advancedSearch = document.getElementById('advanced-search');
     const addRowBtns = advancedSearch.querySelectorAll('.add-value');
     let subFields;
-    let removeRowBtns;
 
     const placeLabels = (e = null) => {
+        if (e && e.target.classList.contains('remove-value')) {
+            const inputsGroup = e.target.parentElement.parentElement;
+            if (!inputsGroup || !inputsGroup.classList.contains('inputs')) {
+                return;
+            }
+
+            const nextAddValueBtn = inputsGroup.nextElementSibling;
+            if (!nextAddValueBtn && !nextAddValueBtn.classList.contains('add-value')) {
+                return;
+            }
+
+            nextAddValueBtn.focus();
+        }
+
         setTimeout(() => {
             if (e && e.target.classList.contains('add-value')) {
                 const inputsGroup = e.target.previousElementSibling;
-                if (!inputsGroup) return;
+                if (!inputsGroup || !inputsGroup.classList.contains('inputs')) {
+                    return;
+                }
 
                 const lastSelectAdded = inputsGroup.querySelector(
                     '.value:last-child select'
                 );
 
-                lastSelectAdded?.focus();
+                lastSelectAdded.focus();
             }
 
             subFields = propertyQueries.querySelectorAll('.sub-field');
             subFields.forEach((subField) => {
                 const prev = subField.previousElementSibling;
-                if(prev.tagName == 'LABEL') {
+                if (prev.tagName == 'LABEL') {
                     prev.style.left = subField.offsetLeft + 'px';
                     prev.style.top = (subField.offsetTop - 19) + 'px';
                     prev.style.opacity = 1;
                 }
             });
 
-            removeRowBtns = propertyQueries.querySelectorAll('.remove-value');
-            removeRowBtns.forEach((removeRowBtn) => {
-                removeRowBtn.addEventListener('click', placeLabels);
+            const removeRowBtns = advancedSearch.querySelectorAll('.remove-value');
+            removeRowBtns.forEach(btn => {
+                btn.addEventListener('click', placeLabels);
             });
         }, 10);
     }
